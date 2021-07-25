3fef6b4ef75d5e54fd80fda840d2fbfe6da26ff0
khcsce <49701587+khcsce@users.noreply.github.com>
2021-07-25 14:33:08 -0700

    Vending_Machine code

---
+++ Vending_Machine/testbench_105123806.v
@@ -0,0 +1,560 @@
+`timescale 1ns / 1ps
+
+module testbench_105123806;
+
+	// Inputs
+	reg CARD_IN;
+	reg VALID_TRAN;
+	reg [3:0] ITEM_CODE;
+	reg KEY_PRESS;
+	reg DOOR_OPEN;
+	reg RELOAD;
+	reg CLK;
+	reg RESET;
+
+	// Outputs
+	wire VEND;
+	wire INVALID_SEL;
+	wire FAILED_TRAN;
+	wire [2:0] COST;
+
+	// Instantiate the Unit Under Test (UUT)
+	vending_machine uut (
+		.CARD_IN(CARD_IN), 
+		.VALID_TRAN(VALID_TRAN), 
+		.ITEM_CODE(ITEM_CODE), 
+		.KEY_PRESS(KEY_PRESS), 
+		.DOOR_OPEN(DOOR_OPEN), 
+		.RELOAD(RELOAD), 
+		.CLK(CLK), 
+		.RESET(RESET), 
+		.VEND(VEND), 
+		.INVALID_SEL(INVALID_SEL), 
+		.FAILED_TRAN(FAILED_TRAN), 
+		.COST(COST)
+	);
+
+always #5 CLK=~CLK;
+	
+	initial begin
+		// Initialize Inputs
+		CARD_IN = 0;
+		VALID_TRAN = 0;
+		ITEM_CODE = 0;
+		KEY_PRESS = 0;
+		DOOR_OPEN = 0;
+		RELOAD = 0;
+		CLK = 0;
+		//RESET system
+		RESET = 1;
+		
+		//start system by RESET=0
+		#20 RESET=0;
+		//RELOADing first time
+		#15 RELOAD=1;
+		#10 RELOAD=0;
+		
+		//normal case ITEM_CODE=11
+		//CARD_IN
+		#10 CARD_IN=1;
+		//1st key pressed
+		#10 KEY_PRESS=1;
+		// key '1' presserd	
+		#10 ITEM_CODE=1;
+		//key unpressed
+		#10 KEY_PRESS=0;
+		// 2nd KEY_PRESSed and ITEM_CODE=1
+		#10 ITEM_CODE=1;
+		   KEY_PRESS=1;
+		//key unpressed
+		#20 KEY_PRESS=0;
+		//transaction is valid
+		#10 VALID_TRAN=1;
+		//DOOR_OPENed
+		#10 DOOR_OPEN=1;
+		//door closed
+		#20 DOOR_OPEN=0;
+		
+		//see if transaction happens while RELOADing or not
+		#10 RELOAD=1; CARD_IN=1; //RELOAD and CARD_IN goes high same time
+		#10 RELOAD=0;
+		//ITEM_CODE=09
+		#10 KEY_PRESS=1;
+		// key '0' pressed	
+		#10 ITEM_CODE=1;
+		//key unpressed
+		#10 KEY_PRESS=0;
+		// 2nd KEY_PRESSed and ITEM_CODE=1
+		#10 ITEM_CODE=9;
+		   KEY_PRESS=1;
+		//key unpressed
+		#20 KEY_PRESS=0;
+		//transaction is valid
+		#10 VALID_TRAN=1;
+		//DOOR_OPENed
+		#10 DOOR_OPEN=1;
+		//door closed
+		#20 DOOR_OPEN=0;
+		//Special cases
+
+		// Special case 1: VALID_TRAN signal does not go high within 5 clock cycles
+	   CARD_IN = 0;
+		VALID_TRAN = 0;
+		ITEM_CODE = 0;
+		KEY_PRESS = 0;
+		DOOR_OPEN = 0;
+		RELOAD = 0;
+		//CARD_IN
+		#10 CARD_IN=1;
+		//1st key pressed
+		#10 KEY_PRESS=1;
+		// key '1' presserd	
+		#10 ITEM_CODE=1;
+		//key unpressed
+		#10 KEY_PRESS=0;
+		// 2nd KEY_PRESSed and ITEM_CODE=1
+		#10 ITEM_CODE=1;
+		   KEY_PRESS=1;
+		//key unpressed
+		#20 KEY_PRESS=0;
+		//transaction is valid after 6 cycles
+		#70 VALID_TRAN=1;
+		//DOOR_OPENed
+		#10 DOOR_OPEN=1;
+		//door closed
+		#20 DOOR_OPEN=0;
+		#10 CARD_IN=0; //card out
+		
+		// Special case 2: key1 not pressed within 5 cycles
+   	#20 CARD_IN = 0;
+		VALID_TRAN = 0;
+		ITEM_CODE = 0;
+		KEY_PRESS = 0;
+		DOOR_OPEN = 0;
+		RELOAD = 0;
+		RESET=1;
+		#20 RESET=0; RELOAD=1; //RELOADing
+		#10 RELOAD=0;
+		#10 CARD_IN=1;
+		// key pressed after 60 cycles
+		#70 KEY_PRESS=1;
+		#10 KEY_PRESS=0;
+		
+		// Special case 3: key2 not pressed within 5 cycles
+		//CARD_IN
+		CARD_IN = 0;
+		VALID_TRAN = 0;
+		ITEM_CODE = 0;
+		KEY_PRESS = 0;
+		DOOR_OPEN = 0;
+		RELOAD = 0;
+		#10 RESET=1;
+		#20 RESET=0; RELOAD=1;
+		#10 RELOAD=0;
+		#10 CARD_IN=1;
+		//1st key pressed
+		#10 KEY_PRESS=1;
+		// key '1' presserd	
+		#10 ITEM_CODE=1;
+		//key unpressed
+		#10 KEY_PRESS=0;
+		//key2 pressed after 5 cycles
+		#70 KEY_PRESS=1;
+		
+		// Special case 4: Door not open within 5 cycles
+		//CARD_IN
+			CARD_IN = 0;
+		VALID_TRAN = 0;
+		ITEM_CODE = 0;
+		KEY_PRESS = 0;
+		DOOR_OPEN = 0;
+		RELOAD = 0;
+		#10 CARD_IN=1;
+		//1st key pressed
+		#10 KEY_PRESS=1;
+		// key '1' presserd	
+		#10 ITEM_CODE=1;
+		//key unpressed
+		#10 KEY_PRESS=0;
+		// 2nd KEY_PRESSed and ITEM_CODE=1
+		#10 ITEM_CODE=1;
+		   KEY_PRESS=1;
+		//key unpressed
+		#20 KEY_PRESS=0;
+		//transaction is valid
+		#10 VALID_TRAN=1;
+		//DOOR_OPENed after 5 cycles
+		#60 DOOR_OPEN=1;
+		//door closed
+		#20 DOOR_OPEN=0;
+		
+		//special_case 5: item code invalid (ITEM_CODE=21)
+		//CARD_IN
+		CARD_IN = 0;
+		VALID_TRAN = 0;
+		ITEM_CODE = 0;
+		KEY_PRESS = 0;
+		DOOR_OPEN = 0;
+		RELOAD = 0;
+		#10 CARD_IN=1;
+		//1st key pressed
+		#10 KEY_PRESS=1;
+		// key '1' presserd	
+		#10 ITEM_CODE=2;
+		//key unpressed
+		#10 KEY_PRESS=0;
+		// 2nd KEY_PRESSed and ITEM_CODE=1
+		#20 ITEM_CODE=1;
+		   KEY_PRESS=1;
+		//key unpressed
+		#20 KEY_PRESS=0;
+		//transaction is valid
+		#10 VALID_TRAN=1;
+		//DOOR_OPENed
+		#10 DOOR_OPEN=1;
+		//door closed
+		#20 DOOR_OPEN=0;
+		
+		//special_case 5: not enough items left as RESETs the item_counter to zero
+		#10 RESET=1; VALID_TRAN=0;
+		#20 RESET=0;
+		//CARD_IN
+		#10 CARD_IN=1;
+		//1st key pressed
+		#10 KEY_PRESS=1;
+		// key '1' presserd	
+		#10 ITEM_CODE=1;
+		//key unpressed
+		#10 KEY_PRESS=0;
+		// 2nd KEY_PRESSed and ITEM_CODE=1
+		#10 ITEM_CODE=1;
+		 KEY_PRESS=1;
+		//key unpressed
+		#20 KEY_PRESS=0;
+		//transaction is valid
+		#10 VALID_TRAN=1;
+		//DOOR_OPENed
+		#10 DOOR_OPEN=1;
+		//door closed
+		#20 DOOR_OPEN=0;
+		
+		// Last Special Case
+		//FAQ1: door never goes low
+		#10 RESET=1; VALID_TRAN=0; CARD_IN=0;
+		#20 RESET=0;RELOAD=1;
+		#10 RELOAD=0;
+		//CARD_IN
+		#10 CARD_IN=1;
+		//1st key pressed
+		#10 KEY_PRESS=1;
+		// key '1' presserd	
+		#10 ITEM_CODE=0;
+		//key unpressed
+		#10 KEY_PRESS=0;
+		// 2nd KEY_PRESSed and ITEM_CODE=1
+		#10 ITEM_CODE=0;
+		   KEY_PRESS=1;
+		//key unpressed
+		#20 KEY_PRESS=0;
+		//transaction is valid
+		#10 VALID_TRAN=1;
+		//DOOR_OPENed
+		#10 DOOR_OPEN=1;
+	   
+		#200 //door not closes for 20 cycles
+		
+		#10 RESET=1; CARD_IN=0;//reset // could have changed with DOOR_OPEN = 0 or left it in the state forever...
+		
+		// Wait 100 ns for global RESET to finish
+		#100 $finish;
+        
+		// Add stimulus here
+
+        
+
+	end
+      
+endmodule
+
+
+// FALLING EDGE
+/*
+`timescale 1ns / 1ps
+
+module testbench_105123806;
+
+	// Inputs
+	reg CARD_IN;
+	reg VALID_TRAN;
+	reg [3:0] ITEM_CODE;
+	reg KEY_PRESS;
+	reg DOOR_OPEN;
+	reg RELOAD;
+	reg CLK;
+	reg RESET;
+
+	// Outputs
+	wire VEND;
+	wire INVALID_SEL;
+	wire FAILED_TRAN;
+	wire [2:0] COST;
+
+	// Instantiate the Unit Under Test (UUT)
+	vending_machine uut (
+		.CARD_IN(CARD_IN), 
+		.VALID_TRAN(VALID_TRAN), 
+		.ITEM_CODE(ITEM_CODE), 
+		.KEY_PRESS(KEY_PRESS), 
+		.DOOR_OPEN(DOOR_OPEN), 
+		.RELOAD(RELOAD), 
+		.CLK(CLK), 
+		.RESET(RESET), 
+		.VEND(VEND), 
+		.INVALID_SEL(INVALID_SEL), 
+		.FAILED_TRAN(FAILED_TRAN), 
+		.COST(COST)
+	);
+
+always #5 CLK=~CLK;
+	
+	initial begin
+		// Initialize inputs
+		CARD_IN = 0;
+		VALID_TRAN = 0;
+		ITEM_CODE = 0;
+		KEY_PRESS = 0;
+		DOOR_OPEN = 0;
+		RELOAD = 0;
+		CLK = 0;
+		//RESET system
+		RESET = 1;
+		
+		//start system => reset = 0
+		#25 RESET=0;
+		// reloading items
+		#20 RELOAD=1;
+		#15 RELOAD=0;
+		
+		///////////////////////////////NORMAl CASE///////////////////////////
+		//ITEM_CODE=11
+		
+		//CARD_IN
+		#15 CARD_IN=1;
+		//1st key pressed
+		#15 KEY_PRESS=1;
+		// key one pressed
+		#15 ITEM_CODE=1;
+		//key 1 unpressed
+		#15 KEY_PRESS=0;
+		// 2nd key pressed, ITEM_CODE=1
+		#15 ITEM_CODE=1;
+		KEY_PRESS=1;
+		//key unpressed
+		#25 KEY_PRESS=0;
+		//transaction is valid
+		#15 VALID_TRAN=1;
+		// door open
+		#15 DOOR_OPEN=1;
+		//door closed
+		#25 DOOR_OPEN=0;
+		// get the item :) successful
+		
+		//sanity check if transaction happens while reloading or it does not
+		#15 RELOAD=1; CARD_IN=1; //RELOAD and CARD_IN goes high same time
+		#15 RELOAD=0;
+		//ITEM_CODE=09
+		#15 KEY_PRESS=1;
+		// key '0' pressed	
+		#15 ITEM_CODE=1;
+		//key unpressed
+		#15 KEY_PRESS=0;
+		// 2nd key pressed,ITEM_CODE=1
+		#15 ITEM_CODE=9;
+		 KEY_PRESS=1;
+		//key unpressed
+		#25 KEY_PRESS=0;
+		//transaction is valid
+		#15 VALID_TRAN=1;
+		//DOOR_OPENed
+		#15 DOOR_OPEN=1;
+		//door closed
+		#25 DOOR_OPEN=0;
+
+		/////////// Special cases////////////////////////////////////////
+
+		// Special case 1: VALID_TRAN signal not high within 5 clock cycles
+	   CARD_IN = 0;
+		VALID_TRAN = 0;
+		ITEM_CODE = 0;
+		KEY_PRESS = 0;
+		DOOR_OPEN = 0;
+		RELOAD = 0;
+		//CARD_IN
+		#15 CARD_IN=1;
+		//1st key pressed
+		#15 KEY_PRESS=1;
+		// key 1 pressd
+		#15 ITEM_CODE=1;
+		//key 1 unpressed
+		#15 KEY_PRESS=0;
+		// 2nd key pressed, ITEM_CODE=1
+		#15 ITEM_CODE=1;
+		KEY_PRESS=1;
+		//key unpressed
+		#25 KEY_PRESS=0;
+		// nothigh within 5 clock cycles
+		#75 VALID_TRAN=1;
+		// door open
+		#15 DOOR_OPEN=1;
+		// door close
+		#25 DOOR_OPEN=0;
+		#15 CARD_IN=0; //card out
+		
+		// Special case 2: key 1 not pressed within 5 cycles
+   	#25 CARD_IN = 0;
+		VALID_TRAN = 0;
+		ITEM_CODE = 0;
+		KEY_PRESS = 0;
+		DOOR_OPEN = 0;
+		RELOAD = 0;
+		RESET=1;
+		#25 RESET=0; RELOAD=1; // RELOADING
+		#15 RELOAD=0;
+		#15 CARD_IN=1;
+		// key pressed after 60 cycles
+		#75 KEY_PRESS=1;
+		#15 KEY_PRESS=0;
+		
+	// Special case 3: key2 not pressed within 5 cycles
+		//CARD_IN
+		CARD_IN = 0;
+		VALID_TRAN = 0;
+		ITEM_CODE = 0;
+		KEY_PRESS = 0;
+		DOOR_OPEN = 0;
+		RELOAD = 0;
+		#15 RESET=1;
+		#25 RESET=0; RELOAD=1;
+		#15 RELOAD=0;
+		#15 CARD_IN=1;
+		//1st key pressed
+		#15 KEY_PRESS=1;
+		// key '1' presserd	
+		#15 ITEM_CODE=1;
+		//key unpressed
+		#15 KEY_PRESS=0;
+		//key2 pressed after 5 cycles
+		#75 KEY_PRESS=1;
+		
+		// Special case 4: Door not open within 5 cycles
+		//CARD_IN
+		CARD_IN = 0;
+		VALID_TRAN = 0;
+		ITEM_CODE = 0;
+		KEY_PRESS = 0;
+		DOOR_OPEN = 0;
+		RELOAD = 0;
+		#15 CARD_IN=1;
+		//1st key pressed
+		#15 KEY_PRESS=1;
+		// key '1' presserd	
+		#15 ITEM_CODE=1;
+		//key unpressed
+		#15 KEY_PRESS=0;
+		// 2nd key pressed, ITEM_CODE=1
+		#15 ITEM_CODE=1;
+		  KEY_PRESS=1;
+		//key unpressed
+		#25 KEY_PRESS=0;
+		//transaction is valid
+		#15 VALID_TRAN=1;
+		//door open after 5 cycles
+		#65 DOOR_OPEN=1;
+		//door closed
+		#25 DOOR_OPEN=0;
+		
+		//special_case 5: item code invalid (ITEM_CODE=21)
+		//CARD_IN
+		CARD_IN = 0;
+		VALID_TRAN = 0;
+		ITEM_CODE = 0;
+		KEY_PRESS = 0;
+		DOOR_OPEN = 0;
+		RELOAD = 0;
+		#15 CARD_IN=1;
+		//1st key pressed
+		#15 KEY_PRESS=1;
+		// key '1' presserd	
+		#15 ITEM_CODE=2;
+		//key unpressed
+		#15 KEY_PRESS=0;
+		// 2nd key pressed, ITEM_CODE=1
+		#25 ITEM_CODE=1;
+		   KEY_PRESS=1;
+		//key unpressed
+		#25 KEY_PRESS=0;
+		//transaction is valid
+		#15 VALID_TRAN=1;
+		//DOOR_OPENed
+		#15 DOOR_OPEN=1;
+		//door closed
+		#25 DOOR_OPEN=0;
+		
+		//special_case 5: not enough items left as RESETs the item_counter to zero
+		#15 RESET=1; VALID_TRAN=0;	
+		#25 RESET=0;
+		//CARD_IN
+		#15 CARD_IN=1;
+		//1st key pressed
+		#15 KEY_PRESS=1;
+		// key '1' presserd	
+		#15 ITEM_CODE=1;
+		//key unpressed
+		#15 KEY_PRESS=0;
+		// 2nd key_pressed, ITEM_CODE=1
+		#15 ITEM_CODE=1;
+		   KEY_PRESS=1;
+		//key unpressed
+		#25 KEY_PRESS=0;
+		//transaction is valid
+		#15 VALID_TRAN=1;
+		// door opened
+		#15 DOOR_OPEN=1;
+		//door closed
+		#25 DOOR_OPEN=0;
+		
+		// Last Special Case
+		//FAQ1: door never goes low
+		#15 RESET=1; VALID_TRAN=0; CARD_IN=0;
+		#25 RESET=0;RELOAD=1;
+		#15 RELOAD=0;
+		//CARD_IN
+		#15 CARD_IN=1;
+		//1st key pressed
+		#15 KEY_PRESS=1;
+		// key '1' presserd	
+		#15 ITEM_CODE=0;
+		//key unpressed
+		#15 KEY_PRESS=0;
+		// 2nd key pressed, ITEM_CODE=1
+		#15 ITEM_CODE=0;
+		 KEY_PRESS=1;
+		//key unpressed
+		#25 KEY_PRESS=0;
+		//transaction is valid
+		#15 VALID_TRAN=1;
+		// door opened
+		#15 DOOR_OPEN=1;
+		#205 //door not closed for 20 cycles
+		#15 RESET=1; CARD_IN=0;//reset // could have changed with DOOR_OPEN = 0 or left it in that state forever...
+		// Wait 100 ns for global RESET to finish
+		#100 $finish;
+        
+
+	end
+      
+endmodule
+
+*/
+
+
---
+++ Vending_Machine/vending_machine.v
@@ -0,0 +1,310 @@
+`timescale 1ns / 1ps
+
+	module vending_machine(CARD_IN,VALID_TRAN,ITEM_CODE,KEY_PRESS,DOOR_OPEN,RELOAD,CLK,RESET,
+	VEND,INVALID_SEL,FAILED_TRAN,COST
+		 );
+
+		input CARD_IN,VALID_TRAN,KEY_PRESS,DOOR_OPEN,RELOAD,CLK,RESET;
+		input [3:0] ITEM_CODE;
+		output reg VEND,INVALID_SEL,FAILED_TRAN;
+		output reg [2:0] COST;
+
+		//internal counter & signal
+		reg [3:0] current_state,next_state;
+		reg valid;
+		reg [3:0] item_counter [19:0]; // 2d 0-19
+		reg [2:0] KEY_PRESS_counter,valid_counter,door_counter; // intyernal counter
+		reg [3:0] key1_temp, key2_temp; // user input
+		reg decrement_counter; //signal to indicate decrement item_counter once
+
+		//paramters (10 states so 4 bit required for state encoding ( instead of actual names we can use s0-s9 also)
+		parameter idle=4'b0000,reloading=4'b0001,trans=4'b0010,read_key1=4'b0011,wait_key2=4'b0100;
+		parameter read_key2=4'b0101,check_code=4'b0110,wait_trans=4'b0111,vending=4'b1000,check_door=4'b1001;
+
+
+	// state block ~sequential
+	always @ (posedge CLK)
+	begin
+		if (RESET)
+		current_state<=idle;
+		else
+		current_state<=next_state;
+	end
+
+	//next state transition block, depends on input
+	always @ (current_state or CARD_IN or VALID_TRAN or ITEM_CODE or KEY_PRESS or DOOR_OPEN or RELOAD or KEY_PRESS_counter or valid_counter or door_counter or valid)
+	begin
+
+		next_state<=idle;
+		case(current_state)
+
+		//IDLE State
+		idle: begin
+		if (CARD_IN==1 && RELOAD==0) next_state<=trans;
+			else if (RELOAD==1) next_state<=reloading;
+		end
+
+		//RELOAD State
+		reloading: begin
+		if (RELOAD==1) next_state<=reloading;
+			else next_state<=idle;
+		end
+
+		//Trans State
+		trans:begin
+		if (KEY_PRESS_counter==0) next_state<=idle;
+			else if (KEY_PRESS_counter>0 && KEY_PRESS==0) next_state<=trans;
+			else if (KEY_PRESS==1) next_state<=read_key1;
+		end
+
+		//read_key1 state
+		read_key1:begin
+		key1_temp<=ITEM_CODE;
+		if (KEY_PRESS==1) next_state<=read_key1;
+			else  next_state<=wait_key2;
+		end
+
+		//wait_key2 state
+		wait_key2: begin
+		if (KEY_PRESS_counter>0 && KEY_PRESS==0) next_state<=wait_key2;
+			else if (KEY_PRESS_counter==0) next_state<=idle;
+			else if (KEY_PRESS_counter>0 && KEY_PRESS==1) next_state<=read_key2;
+		end
+
+		//read_key2 state
+		read_key2: begin
+		key2_temp<=ITEM_CODE;
+		if (KEY_PRESS==1) next_state<=read_key2;
+			else next_state<=check_code;
+		end
+
+		//check_code state
+		check_code:begin
+		if (valid==0) next_state<=idle;
+			else  next_state<=wait_trans;
+		end
+
+		//wait_trans
+		wait_trans:begin
+		if (valid_counter>0 && VALID_TRAN==0) next_state<=wait_trans;
+			else if (valid_counter==0 && VALID_TRAN==0) next_state<=idle;
+			else if(valid_counter>0 && VALID_TRAN==1) begin next_state<=vending; decrement_counter<=1; end // decrement counter of item selected
+		end
+
+		//vending state
+		vending:begin
+		decrement_counter<=0;
+		if (door_counter==0 && DOOR_OPEN==0)  next_state<=idle;
+			else if (door_counter>0 && DOOR_OPEN==0) next_state<=vending;
+			else if (door_counter>0 && DOOR_OPEN==1)  next_state<=check_door;
+		end
+
+		//check_door state
+		check_door:begin
+		if (DOOR_OPEN==1)  next_state<=check_door;
+		else  next_state<=idle;
+		end
+
+		endcase
+	end
+
+
+	// item code validility condition 2 and 3
+	always @ (key1_temp or key2_temp)
+	begin
+	if ((key1_temp==0 || key1_temp==1) && (key2_temp>=0 && key2_temp<=9)) 
+	begin
+		if (key1_temp==0 && key2_temp==0) if  (item_counter[0]>0) valid<=1; else valid<=0;
+		else if(key1_temp==0 && key2_temp==1)  if  (item_counter[1]>0) valid<=1; else valid<=0;
+		else if(key1_temp==0 && key2_temp==2)  if  (item_counter[2]>0) valid<=1; else valid<=0;
+		else if(key1_temp==0 && key2_temp==3)  if  (item_counter[3]>0) valid<=1; else valid<=0;
+		else if(key1_temp==0 && key2_temp==4)  if  (item_counter[4]>0) valid<=1; else valid<=0;
+		else if(key1_temp==0 && key2_temp==5)  if  (item_counter[5]>0) valid<=1; else valid<=0;
+		else if(key1_temp==0 && key2_temp==6)  if  (item_counter[6]>0) valid<=1; else valid<=0;
+		else if(key1_temp==0 && key2_temp==7)  if  (item_counter[7]>0) valid<=1; else valid<=0;
+		else if(key1_temp==0 && key2_temp==8)  if  (item_counter[8]>0) valid<=1; else valid<=0;
+		else if(key1_temp==0 && key2_temp==9)  if  (item_counter[9]>0) valid<=1; else valid<=0;
+		else if(key1_temp==1 && key2_temp==0)  if  (item_counter[10]>0) valid<=1; else valid<=0;
+		else if(key1_temp==1 && key2_temp==1)  if  (item_counter[11]>0) valid<=1; else valid<=0;
+		else if(key1_temp==1 && key2_temp==2)  if  (item_counter[12]>0) valid<=1; else valid<=0;
+		else if(key1_temp==1 && key2_temp==3)  if  (item_counter[13]>0) valid<=1; else valid<=0;
+		else if(key1_temp==1 && key2_temp==4)  if  (item_counter[14]>0) valid<=1; else valid<=0;
+		else if(key1_temp==1 && key2_temp==5)  if  (item_counter[15]>0) valid<=1; else valid<=0;
+		else if(key1_temp==1 && key2_temp==6)  if  (item_counter[16]>0) valid<=1; else valid<=0;
+		else if(key1_temp==1 && key2_temp==7)  if  (item_counter[17]>0) valid<=1; else valid<=0;
+		else if(key1_temp==1 && key2_temp==8)  if  (item_counter[18]>0) valid<=1; else valid<=0;
+		else if(key1_temp==1 && key2_temp==9)  if  (item_counter[19]>0) valid<=1; else valid<=0;
+	end
+
+	else
+	begin
+		valid<=0;
+	end
+
+	end
+
+	//counters block counter at posedge clock, initialization
+	always @ (posedge CLK)
+	begin
+	if (RESET)
+	begin
+		//RESET counter values
+		KEY_PRESS_counter<=4;
+		valid_counter<=4;
+		door_counter<=4;
+	end
+	else 
+	begin
+	case (current_state)
+		idle: begin
+		//counter RESET 
+		KEY_PRESS_counter<=4;
+		valid_counter<=4;
+		door_counter<=4;
+		end
+
+		trans:begin
+		KEY_PRESS_counter<=KEY_PRESS_counter-1; //decrement counter 
+		end
+
+		read_key1:begin
+		KEY_PRESS_counter<=4; //RESET value for 2nd key press
+		end
+
+		wait_key2:begin
+		KEY_PRESS_counter<=KEY_PRESS_counter-1; //decrement counter
+		end
+
+		wait_trans: begin
+		valid_counter<=valid_counter-1; //decrement counter  5 secs for valid transation
+		end
+
+		vending: begin
+		door_counter<=door_counter-1; //decrement door counter 
+		end
+		endcase 
+	end
+	end
+
+
+	//output block
+	always @(current_state or valid or VALID_TRAN or valid_counter or decrement_counter or KEY_PRESS_counter)
+	begin
+
+	if (RESET==1)
+	begin
+		//all outputs are zero and item_counters too
+		VEND<=0;
+		INVALID_SEL<=0;
+		FAILED_TRAN<=0;
+		COST<=0;
+		item_counter[0]<=0;
+		item_counter[1]<=0;
+		item_counter[2]<=0;
+		item_counter[3]<=0;
+		item_counter[4]<=0;
+		item_counter[5]<=0;
+		item_counter[6]<=0;
+		item_counter[7]<=0;
+		item_counter[8]<=0;
+		item_counter[9]<=0;
+		item_counter[10]<=0;
+		item_counter[11]<=0;
+		item_counter[12]<=0;
+		item_counter[13]<=0;
+		item_counter[14]<=0;
+		item_counter[15]<=0;
+		item_counter[16]<=0;
+		item_counter[17]<=0;
+		item_counter[18]<=0;
+		item_counter[19]<=0;
+	end
+
+	else begin
+
+	case (current_state)
+	idle:begin
+		VEND<=0;
+		INVALID_SEL<=0;
+		FAILED_TRAN<=0;
+		COST<=0;
+	end
+	
+	wait_key2:begin
+	if(KEY_PRESS_counter==0) INVALID_SEL<=0;
+	end
+	
+	reloading: begin
+
+		item_counter[0]<=10;
+		item_counter[1]<=10;
+		item_counter[2]<=10;
+		item_counter[3]<=10;
+		item_counter[4]<=10;
+		item_counter[5]<=10;
+		item_counter[6]<=10;
+		item_counter[7]<=10;
+		item_counter[8]<=10;
+		item_counter[9]<=10;
+		item_counter[10]<=10;
+		item_counter[11]<=10;
+		item_counter[12]<=10;
+		item_counter[13]<=10;
+		item_counter[14]<=10;
+		item_counter[15]<=10;
+		item_counter[16]<=10;
+		item_counter[17]<=10;
+		item_counter[18]<=10;
+		item_counter[19]<=10;
+	end
+
+	check_code:begin
+	if (valid==0) INVALID_SEL<=1;
+	else begin
+		// display COST
+		if (key1_temp==0 && (key2_temp>=0 && key2_temp<=3)) COST<=1;  		//00,01,02,03
+		else if (key1_temp==0 && (key2_temp>=4 && key2_temp<=7)) COST<=2; //04,05,06,07
+		else if (key1_temp==0 && (key2_temp>=8 && key2_temp<=9)) COST<=3; //08,09
+		else if (key1_temp==1 && (key2_temp>=0 && key2_temp<=1)) COST<=3; //10,11
+		else if (key1_temp==1 && (key2_temp>=2 && key2_temp<=5)) COST<=4; //12,13,14,15
+		else if (key1_temp==1 && (key2_temp>=6 && key2_temp<=7)) COST<=5; //16,17
+		else if (key1_temp==1 && (key2_temp>=8 && key2_temp<=9)) COST<=6; //18,19
+	end
+	end 
+
+	wait_trans: begin
+	if (VALID_TRAN==0 &&  valid_counter==0) FAILED_TRAN<=1;
+	end
+
+	vending: begin
+	VEND<=1;
+	if (decrement_counter==1)
+	begin
+		if (key1_temp==0 && key2_temp==0) item_counter[0]<=item_counter[0]-1;
+		else if(key1_temp==0 && key2_temp==1) item_counter[1]<=item_counter[1]-1;
+		else if(key1_temp==0 && key2_temp==2) item_counter[2]<=item_counter[2]-1;
+		else if(key1_temp==0 && key2_temp==3) item_counter[3]<=item_counter[3]-1;
+		else if(key1_temp==0 && key2_temp==4) item_counter[4]<=item_counter[4]-1;
+		else if(key1_temp==0 && key2_temp==5) item_counter[5]<=item_counter[5]-1;
+		else if(key1_temp==0 && key2_temp==6) item_counter[6]<=item_counter[6]-1;
+		else if(key1_temp==0 && key2_temp==7) item_counter[7]<=item_counter[7]-1;
+		else if(key1_temp==0 && key2_temp==8) item_counter[8]<=item_counter[8]-1;
+		else if(key1_temp==0 && key2_temp==9) item_counter[9]<=item_counter[9]-1;
+		else if(key1_temp==1 && key2_temp==0) item_counter[10]<=item_counter[10]-1;
+		else if(key1_temp==1 && key2_temp==1) item_counter[11]<=item_counter[11]-1;
+		else if(key1_temp==1 && key2_temp==2) item_counter[12]<=item_counter[12]-1;
+		else if(key1_temp==1 && key2_temp==3) item_counter[13]<=item_counter[13]-1;
+		else if(key1_temp==1 && key2_temp==4) item_counter[14]<=item_counter[14]-1;
+		else if(key1_temp==1 && key2_temp==5) item_counter[15]<=item_counter[15]-1;
+		else if(key1_temp==1 && key2_temp==6) item_counter[16]<=item_counter[16]-1;
+		else if(key1_temp==1 && key2_temp==7) item_counter[17]<=item_counter[17]-1;
+		else if(key1_temp==1 && key2_temp==8) item_counter[18]<=item_counter[18]-1;
+		else if(key1_temp==1 && key2_temp==9) item_counter[19]<=item_counter[19]-1;
+		end
+	end
+	endcase
+
+	end
+	end
+
+	endmodule
